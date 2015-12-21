f77c407c2e6969ee4a229a6d5b74919f7c984053
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2015-12-20 22:32:22 -0800

    such server

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 // libraries
 import _ from 'lodash';
 import Bottle from 'bottlejs';
+import Redis from 'ioredis';
+import winston from 'winston';
 // local
 import { mergeConfiguration } from './utils/config';
 import Server from './server';
@@ -11,13 +13,18 @@ const bottle = new Bottle();
 const configuration = mergeConfiguration(process.env, {
     HTTP_PORT: 8080,
     NODE_ENV: 'dev',
+    TWITTER_API_KEY: '',
+    TWITTER_API_SECRET: '',
 });
 
 // define configuration options in bottle
 _.forEach(configuration, (value, key) => bottle.value(key, value));
 
+winston.level = configuration.NODE_ENV == 'dev' ? 'debug' : 'warn';
+
 // define bottle services
-bottle.service('server', Server, 'HTTP_PORT');
+bottle.service('redis', Redis);
+bottle.service('server', Server, 'HTTP_PORT', 'NODE_ENV', 'TWITTER_API_KEY', 'TWITTER_API_SECRET', 'redis');
 
 // start the server
 bottle.container.server.start().then(stop => {
--- app/server/index.js
+++ app/server/index.js
@@ -1,11 +1,75 @@
 import express from 'express';
+import logger from 'winston';
 import morgan from 'morgan';
+import request from 'request';
 
-export default (HTTP_PORT, NODE_ENV) => {
+function getBearerToken(redis, TWITTER_API_KEY, TWITTER_API_SECRET) {
+    return redis.get('twitter/bearer_token').then((result) => {
+        if (result !== null) {
+            // return a promise with the result inside
+            logger.debug('redis has a bearer token, sticking with that');
+            return new Promise((resolve, reject) => {
+                resolve(result);
+            });
+        } else {
+            logger.debug("redis does not have a bearer token, acquiring new oauth token from twitter");
+
+            return new Promise((resolve, reject) => {
+                request.post({
+                    'url': 'https://api.twitter.com/oauth2/token',
+                    'auth': {
+                        user: TWITTER_API_KEY,
+                        pass: TWITTER_API_SECRET,
+                        sendImmediately: true,
+                    },
+                    'form': {
+                        'grant_type': 'client_credentials'
+                    },
+                }, (error, response, body) => {
+                    if (!error) {
+                        const accessToken = JSON.parse(body)['access_token'];
+
+                        logger.debug('successfully retrieved oauth access token from twitter');
+
+    //                  set key with TTL of 5 minutes
+                        redis.multi().set('twitter/bearer_token', accessToken).expire('twitter/bearer_token', 300)
+                            .exec();
+
+    //                  return promise with access token
+                        resolve(accessToken);
+                    } else {
+                        logger.warn('unable to get oauth access token from twitter');
+                        logger.debug(body.toString());
+                        reject(error, response, body);
+                    }
+                });
+            });
+        }
+    });
+}
+
+export default (HTTP_PORT, NODE_ENV, TWITTER_API_KEY, TWITTER_API_SECRET, redis) => {
     // magic?
     const app = express();
     app.use('/', express.static(__dirname + '../../../public'));
     app.use(morgan(NODE_ENV !== 'dev' ? 'combined' : 'dev'));
+
+    app.get('/api/wow!', (req, resp) => {
+        getBearerToken(redis, TWITTER_API_KEY, TWITTER_API_SECRET).then((token, error) => {
+            logger.debug('requesting doge tweets');
+            request.get({
+                'url': 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=DogeTheDog',
+                'auth': {
+                    bearer: token,
+                    sendImmediately: true,
+                }
+            }, (error, response, body) => {
+                if (!error) {
+                    resp.status(200).json(body);
+                }
+            });
+        });
+    });
 
     return {
         start() {
