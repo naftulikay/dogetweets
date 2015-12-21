8aed98ed81c0d162d8f8b256c0d1bce5e4fb8014
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2015-12-21 00:13:37 -0800

    such title such clean

--- app/bootstrap/index.js
+++
@@ -1,6 +0,0 @@
-import loader from './loader';
-
-export default (app) => {
-    app.loader = loader;
-    app.loader.autoload(`${__dirname}/../controllers`, app);
-};
--- app/routes/index.js
+++
@@ -1,7 +0,0 @@
-export default (app) => {
-    app.get('/', app.MainController.index);
-
-    app.get('/*', (req, res) => {
-        res.send(404);
-    });
-};
